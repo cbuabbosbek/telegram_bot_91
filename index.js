9e8345f75a823f9d9039345fb51e75db39d37944
abbossth <a@test.ru>
2025-10-24 17:05:17 +0500

    on message is done

---
+++ index.js
@@ -0,0 +1,10 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = "8473837135:AAHZqsGdnGkhgzpGUH00UPhHy1O85rT5pIg";
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on("message", function (msg) {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Salom");
+});
