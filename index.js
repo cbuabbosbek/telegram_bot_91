23347b685360086a9acde65f5b12e5262b439bf3
abbossth <a@test.ru>
2025-10-27 18:17:22 +0500

    keyboard

--- index.js
+++ index.js
@@ -6,7 +6,29 @@ const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.on("message", function (msg) {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Salom");
+  const text = msg.text;
+  const firstname = msg.chat.first_name;
+  if (text == "/start") {
+    bot.sendMessage(chatId, `Xush kelibsiz, ${firstname}`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Boshlash 🔥" }],
+          [{ text: "Menu 🥩" }, { text: "Sozlamalar ⚙️" }],
+        ],
+        resize_keyboard: true,
+      },
+    });
+  } else if (text == "Boshlash 🔥") {
+    bot.sendMessage(chatId, "Boshlanyaptii....");
+  } else if (text == "Menu 🥩") {
+    bot.sendMessage(chatId, "Menyuga xush kelibsiz....");
+  } else if (text == "Sozlamalar ⚙️") {
+    bot.sendMessage(chatId, "Sozlamalar xush kelibsiz ⚙️....");
+  } else {
+    bot.sendMessage(chatId, "❗️ Xatolik, iltimos /start tugmasini bosing... ");
+  }
 });
 
-console.log("Bot ishga tushdi");
+// npm install nodemon --save-dev
+
+console.log("Bot ishga tushdi  ");
